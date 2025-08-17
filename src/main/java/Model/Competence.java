c83feea29cf715cab9db841286bc958b9c008272
Sacha <sachaterrasson@gmail.com>
2025-08-17 17:57:13 +0200

    Competence model updated to include competence detail file (name)

--- src/main/java/Model/Competence.java
+++ src/main/java/Model/Competence.java
@@ -1,15 +1,20 @@
 package Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Objects;
 
 public class Competence {
     private String name;
+    @JsonProperty("detail_file_name")
+    private String detailFileName;
 
     public Competence() {
     }
 
-    public Competence(String name) {
+    public Competence(String name, String detailFileName) {
         this.name = name;
+        this.detailFileName = detailFileName;
     }
 
     public String getName() {
@@ -20,15 +25,23 @@ public class Competence {
         this.name = name;
     }
 
+    public String getDetailFileName() {
+        return detailFileName;
+    }
+
+    public void setDetailFileName(String detailFileName) {
+        this.detailFileName = detailFileName;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Competence that = (Competence) o;
-        return Objects.equals(name, that.name);
+        return Objects.equals(name, that.name) && Objects.equals(detailFileName, that.detailFileName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(name);
+        return Objects.hashCode(name) ^ Objects.hashCode(detailFileName);
     }
 }
